7ff393be930efa6a3a9fecb6116761e2976503d0
UmerAnsari-developer <ua7088807748@gmail.com>
2025-12-15 23:09:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace PracticeProject
+{
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello i am Umer Ansari");
+        }
+    }
+}
