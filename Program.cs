6a7e8d1af731ff435ecda0e2d88d28d6d043755b
UmerAnsari-developer <ua7088807748@gmail.com>
2025-12-27 13:18:50 +0530

    Calling statements

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using PracticeProject._01_basic;
 
 namespace PracticeProject
 {
-    internal class Program
+    internal class Program : OOP_Intro
     {
          static void Main(string[] args)
         {
@@ -24,8 +24,28 @@ namespace PracticeProject
             StringMethods sm = new StringMethods();
             //sm.stringMethodsDemo();
             Conditional cs = new Conditional();
-            cs.if_switchDemo();
-            cs.loopsDemo();
+            //cs.if_switchDemo();
+            //cs.loopsDemo();
+
+            Method md = new Method();
+            int num1, num2;
+            Console.WriteLine("Enter first number: ");
+            num1= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            num2= Convert.ToInt32(Console.ReadLine());
+            md.MethodDemo(num1,num2);
+            //md.Farewell("Umer"); // This line will cause a compile-time error due to access modifier
+            md.DemoAccess();
+            //md.ProtectedDemo();// This line will cause a compile-time error due to access modifier
+
+            OOP_Intro oOP_Intro = new OOP_Intro();
+            oOP_Intro.greeting("Umer");
+            //oOP_Intro.display("Umer"); // This line will cause a compile-time error due to access modifier
+            oOP_Intro.DemoAccess();
+            //oOP_Intro.ProtectedDemo(); // This line will cause a compile-time error due to access modifier
+            //oOP_Intro.Farewell("Umer"); // This line will cause a compile-time error due to access modifier
+            oOP_Intro.hello();
+            //oOP_Intro.ProtectedDemo(); 
 
 
         }
